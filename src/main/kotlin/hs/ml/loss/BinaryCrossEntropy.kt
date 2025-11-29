821873a6b12bc007f6bb6108196d35838dc4c024
YEAHx4 <developerstarlight@gmail.com>
2025-11-29 17:53:03 +0900

    feat: mean squared error, binary cross entropy loss

---
+++ src/main/kotlin/hs/ml/loss/BinaryCrossEntropy.kt
@@ -0,0 +1,24 @@
+package hs.ml.loss
+
+import hs.ml.math.Tensor
+
+class BinaryCrossEntropy: Loss {
+    override fun compute(yTrue: Tensor, yPred: Tensor): Double {
+        require(yTrue.shape.second == 1 && yPred.shape.second == 1) {
+            "BinaryCrossEntropy can only be computed for single-output tensors."
+        }
+        require(yTrue.shape.first == yPred.shape.first) {
+            "The number of samples in yTrue and yPred must be the same."
+        }
+
+        val n = yTrue.shape.first
+        var sumCrossEntropy = 0.0
+        for (i in 0 until n) {
+            val yT = yTrue[i, 0]
+            val yP = yPred[i, 0]
+            sumCrossEntropy += - (yT * kotlin.math.ln(yP + 1e-15) + (1 - yT) * kotlin.math.ln(1 - yP + 1e-15))
+        }
+
+        return sumCrossEntropy / n
+    }
+}
--- src/main/kotlin/hs/ml/main.kt
+++ src/main/kotlin/hs/ml/main.kt
@@ -3,10 +3,12 @@ package hs.ml
 import hs.ml.importer.CsvImporter
 import hs.ml.importer.DataImporter
 import hs.ml.importer.LinearDataGenerator
+import hs.ml.loss.MeanSquaredError
 import hs.ml.metric.RootMeanSquaredError
 import hs.ml.model.LinearRegressor
 import hs.ml.scaler.StandardScaler
 import hs.ml.train.ModelFactory
+import hs.ml.train.optimizer.SGD
 import hs.ml.util.formatBytes
 import java.io.File
 import java.util.Scanner
@@ -19,51 +21,19 @@ fun main() {
     println("Mem : ${formatBytes(Runtime.getRuntime().maxMemory())}")
     println()
 
-    val scanner = Scanner(System.`in`)
-    var input = -1
-
-    println("**데이터 입력 단계**")
-    println("1. CSV파일 불러오기")
-    println("2. 랜덤 선형 데이터 생성하기")
-
-    do {
-        print(">> ")
-        input = scanner.nextInt()
-    } while (input != 1 && input != 2)
-
-    var importer: DataImporter
-    do {
-        if (input == 1) {
-            print("CSV 파일 경로 : ")
-            scanner.nextLine() // 버퍼 비우기
-            val path = scanner.nextLine()
-            importer = CsvImporter(path)
-        } else {
-            print("데이터 개수 : ")
-            val n = scanner.nextInt()
-            print("기울기 : ")
-            val slope = scanner.nextDouble()
-            print("절편 : ")
-            val bias = scanner.nextDouble()
-            print("노이즈 : ")
-            val noise = scanner.nextDouble()
-            importer = LinearDataGenerator(n, slope, bias, noise)
-        }
-    } while (!importer.available())
+    val importer = CsvImporter("data/housing.csv")
 
     println("데이터 불러오는 중...")
     val (x, y) = importer.read()
     println("데이터 불러오기 완료!")
     println("x: ${x.shape}, y: ${y.shape}")
 
-    println("\n================================\n\n")
-
     println("**모델 선택 단계**")
 
     val model = ModelFactory.create<LinearRegressor>()
         .setScaler(StandardScaler())
-//        .setLoss(())
+        .setLoss(MeanSquaredError())
+        .setOptimizer(SGD(lr = 0.01))
         .addMetric(RootMeanSquaredError())
         .getModel()
-    // TODO : Model Selection
 }
