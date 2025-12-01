31631688ca4d7cd522d57f66c13ec96f62141524
YEAHx4 <developerstarlight@gmail.com>
2025-12-01 12:49:49 +0900

    feat: use data pipeline and preprocessor

--- src/main/kotlin/hs/ml/main.kt
+++ src/main/kotlin/hs/ml/main.kt
@@ -1,17 +1,17 @@
 package hs.ml
 
+import hs.ml.data.DataPipeline
 import hs.ml.importer.CsvImporter
-import hs.ml.importer.DataImporter
-import hs.ml.importer.LinearDataGenerator
 import hs.ml.loss.MeanSquaredError
 import hs.ml.metric.RootMeanSquaredError
 import hs.ml.model.LinearRegressor
+import hs.ml.preprocessing.DataPreprocessor
+import hs.ml.preprocessing.policy.ReplaceToAvgPolicy
 import hs.ml.scaler.StandardScaler
 import hs.ml.train.ModelFactory
 import hs.ml.train.optimizer.SGD
 import hs.ml.util.formatBytes
 import java.io.File
-import java.util.Scanner
 
 fun main() {
     println("\n\n================================")
@@ -19,16 +19,23 @@ fun main() {
     println("PWD : ${File(".").canonicalFile}")
     println("CPU : ${Runtime.getRuntime().availableProcessors()} cores")
     println("Mem : ${formatBytes(Runtime.getRuntime().maxMemory())}")
+    println("================================\n\n")
     println()
 
-    val importer = CsvImporter("data/housing.csv")
-
+    println("**데이터 불러오기 및 전처리 단계**\n")
     println("데이터 불러오는 중...")
-    val (x, y) = importer.read()
+
+    val importer = CsvImporter("data/housing.csv")
+    val pipeline = DataPipeline(
+        importer = importer,
+        preprocessor = DataPreprocessor(missingPolicy = ReplaceToAvgPolicy())
+    )
+    val (x, y) = pipeline.run()
+
     println("데이터 불러오기 완료!")
     println("x: ${x.shape}, y: ${y.shape}")
 
-    println("**모델 선택 단계**")
+    println("\n**모델 선택 단계**\n")
 
     val model = ModelFactory.create<LinearRegressor>()
         .setScaler(StandardScaler())
@@ -36,4 +43,6 @@ fun main() {
         .setOptimizer(SGD(lr = 0.01))
         .addMetric(RootMeanSquaredError())
         .getModel()
+
+    println("모델 생성 완료! : $model")
 }
